626b76888606aff6adc7f26255747da54f1793c5
Kevin Fang <kfang1233@gmail.com>
2018-03-12 17:00:08 -0400

    Added comments to condor generation

--- generate_condor_job.py
+++ generate_condor_job.py
@@ -2,19 +2,26 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import os, stat
 
+# get project name and file name of feb file
 project_name = raw_input("Name of project (no spaces - i.e. cube): ")
 filename = raw_input("Name of .feb file: ")
-if ".feb" not in filename:
+if ".feb" not in filename: # append the .feb extension if necessary
     filename += ".feb"
+
+# generate names for condor job creation
 script_name = project_name + ".sh"
 job_name = project_name + ".sub"
 log_name = project_name + ".log"
 output_name = project_name + ".output"
 
-
+# write the script to a file
 with open(script_name, "w") as script_file:
     script_file.write("#!/bin/bash\n/home/medialab/febio-2.6.4/bin/febio2.lnx64 -i " + filename + '\n')
+# make the script executable
+st = os.stat(script_name)
+os.chmod(script_name, st.st_mode | stat.S_IEXEC | stat.S_IXGRP | stat.S_IXOTH)
 
+# generate the condor job
 with open(job_name, "w") as job_file:
     job_file.write("""executable = {0}
         log = {1}
@@ -25,7 +32,4 @@ with open(job_name, "w") as job_file:
         should_transfer_files = Yes
         queue\n""".format(script_name, log_name, output_name, filename, filename))
 
-
-st = os.stat(script_name)
-os.chmod(script_name, st.st_mode | stat.S_IEXEC | stat.S_IXGRP | stat.S_IXOTH)
 print("Successfully created condor job files. Script is located at {}, submission is located at {}".format(script_name, job_name))
