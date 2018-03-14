d846aeae69e6fbe9b94585913fe15e7648bc9481
Kevin Fang <kfang1233@gmail.com>
2018-03-14 14:21:23 -0400

    Added Python script to generate cluster job from a folder of feb files

--- generate_condor_job.py
+++ generate_condor_job.py
@@ -7,6 +7,13 @@ project_name = raw_input("Name of project (no spaces - i.e. cube): ")
 filename = raw_input("Name of .feb file: ")
 if ".feb" not in filename: # append the .feb extension if necessary
     filename += ".feb"
+machine_req = raw_input("Set machine requirements? [Y/n] ").lower()
+ram_req = 0
+cpu_req = 0
+if machine_req == "yes" or machine_req == "y":
+    ram_req = input("RAM requirement (MB): ")
+    cpu_req = input("CPU requirement (# cores): ")
+
 
 # generate names for condor job creation
 script_name = project_name + ".sh"
@@ -30,8 +37,10 @@ log = {1}
 output = {2}
 arguments = \"{3}\"
 transfer_input_files = {4}
+request_memory = {5}
+request_cpus = {6}
 error = error.txt
 should_transfer_files = Yes
-queue\n""".format(script_name, log_name, output_name, filename, filename))
+queue\n""".format(script_name, log_name, output_name, filename, filename, ram_req, cpu_req))
 
 print("Successfully created condor job files. Script is located at {}, submission is located at {}".format(script_name, job_name))
---
+++ generate_from_directory.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+from __future__ import absolute_import, division, print_function, unicode_literals
+import os, stat, errno
+
+# get project name and file name of feb file
+project_name = raw_input("Set a project name: ")
+folder_name = raw_input("Location of folder containing .feb files: ")
+machine_req = raw_input("Set machine requirements? [Y/n] ").lower()
+ram_req = 0
+cpu_req = 0
+if machine_req == "yes" or machine_req == "y":
+        ram_req = input("RAM requirement (MB): ")
+        cpu_req = input("CPU requirement (# cores): ")
+
+
+# location where FEBio is located
+FEBIO_LOCATION = '/home/medialab/febio-2.6.4/bin/febio2.lnx64'
+script_name = "febio.sh"
+
+# make sure that an output directory is generated, because HTCondor does not automatically do it
+try:
+    os.makedirs('./output')
+    print("Creating output folder...")
+except OSError as e:
+    if e.errno == errno.EEXIST:
+        print("Output folder already exists. Continuing...")
+    else:
+        raise
+
+# write the FEBio script
+with open(script_name, "w") as script_file:
+    script_file.write("#!/bin/bash\n" + FEBIO_LOCATION + " -i $1 -o $1")
+    
+# make the script executable
+st = os.stat(script_name)
+os.chmod(script_name, st.st_mode | stat.S_IEXEC | stat.S_IXGRP | stat.S_IXOTH)
+
+# generate a job from a filenmae
+def generate_job(filename, input_dir):
+    truncated_filename = filename[0:-4]
+    return """arguments = {filename}
+transfer_input_files = {input_dir}/{filename}
+output = output/{truncated_filename}.out
+error = output/{truncated_filename}.err
+log = output/{truncated_filename}.log
+transfer_output_remaps = \"{truncated_filename}.xplt=./output/{truncated_filename}.xplt; {truncated_filename}.log=./output/{truncated_filename}.log\"
+queue
+
+""".format(filename = filename, truncated_filename = truncated_filename, input_dir = input_dir)
+
+job_name = project_name + ".sub"
+
+# generate the condor job
+with open(job_name, "w") as job_file:
+    job_file.write(
+""" #initialize
+executable = febio.sh
+universe = Vanilla
+should_transfer_files = Yes
+
+""")
+    # iterate through the feb directory, adding a job for each one
+    for filename in os.listdir(folder_name):
+        if filename.endswith(".feb"):
+            job_file.write(generate_job(filename, folder_name))
+        else:
+            print("Skipping " + filename + " (doesn't end in .feb) ")
+
+print("\nSuccessfully generated job. Run condor_submit {} to start the job.".format(job_name))
